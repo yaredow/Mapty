4f2dd196751ca92a2a7b5fe3e894264442396109
yaredow <yareyilma11@gmail.com>
2023-05-30 15:32:13 +0300

    finish handlin the change in input

--- script.js
+++ script.js
@@ -12,26 +12,59 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
 // Getting the current location of user
+
+let map, mapEvent;
 navigator.geolocation.getCurrentPosition(
   function (position) {
-    console.log(position);
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    // console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
     const coords = [latitude, longitude];
-    const map = L.map('map').setView(coords, 13);
+    map = L.map('map').setView(coords, 13);
 
     L.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    L.marker(coords)
-      .addTo(map)
-      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-      .openPopup();
+    // handling clicking events
+    map.on('click', function (mapE) {
+      mapEvent = mapE;
+      form.classList.remove('hidden');
+      inputDistance.focus();
+    });
   },
   function () {
     alert("Couldn't get current position");
   }
 );
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  //   clear input field
+  inputDistance.value = inputDuration.value = inputCadence.value = '';
+  console.log(mapEvent);
+  const lat = mapEvent.latlng.lat;
+  const lng = mapEvent.latlng.lng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('🏃🏽 Running')
+    .openPopup();
+});
+
+// handling change input
+inputType.addEventListener('input', function (e) {
+  e.preventDefault();
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+});
